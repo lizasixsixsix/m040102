9d327f1c11339d4942cd61cb065288170b73d2d2
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-09 01:09:46 +0300

    localized string resources added

--- m040102/Program.cs
+++ m040102/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace m040102
+﻿using System;
+
+namespace m040102
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Properties.Strings.Hello);
         }
     }
 }
