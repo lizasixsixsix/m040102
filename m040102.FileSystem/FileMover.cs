38c70511a18db4535486240785591bbf8ed6ea03
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-09 12:24:34 +0300

    FileMover ctor added

--- m040102.FileSystem/FileMover.cs
+++ m040102.FileSystem/FileMover.cs
@@ -1,9 +1,52 @@
-﻿namespace m040102.FileSystem
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace m040102.FileSystem
 {
     public class FileMover
     {
-        public FileMover()
+        private readonly IEnumerable<DirectoryInfo> directories;
+        private readonly IReadOnlyDictionary<Regex, DirectoryInfo> patternsDictionary;
+        private readonly DirectoryInfo defaultDirectory;
+
+        private readonly IList<FileSystemWatcher> fileSystemWatchers;
+
+        public FileMover(IEnumerable<DirectoryInfo> directories,
+                         IReadOnlyDictionary<Regex, DirectoryInfo> patternsDictionary,
+                         DirectoryInfo defaultDirectory)
         {
+
+            #region ParametersValidation
+
+            this.directories = directories ?? throw new ArgumentNullException();
+
+            if (directories.Any(d => !d.Exists))
+            {
+                throw new ArgumentException();
+            }
+
+            this.patternsDictionary = patternsDictionary ?? throw new ArgumentNullException();
+
+            if (this.patternsDictionary.Select(p => p.Value).Any(d => !d.Exists))
+            {
+                throw new ArgumentException();
+            }
+
+            this.defaultDirectory = defaultDirectory ?? throw new ArgumentNullException();
+
+            if (!this.defaultDirectory.Exists)
+            {
+                throw new ArgumentException();
+            }
+
+            #endregion
+
+            this.fileSystemWatchers = new List<FileSystemWatcher>(
+                this.directories.Select(
+                    d => new FileSystemWatcher(d.FullName)));
         }
     }
 }
